aa809bd1fc9e3c7995cc47f0da4b89f5b17cee4a
Marc Hannappel <marc.hannappel@lise.de>
2022-11-02 11:48:15 +0100

    Implement Handler for validation
    - Implemented an exception handler for validation exceptions, that takes the thrown exceptions and makes them more human readable

--- backend/src/main/java/de/frostforge/backend/tags/controller/TagController.java
+++ backend/src/main/java/de/frostforge/backend/tags/controller/TagController.java
@@ -3,10 +3,15 @@ package de.frostforge.backend.tags.controller;
 import de.frostforge.backend.tags.domain.CreateTag;
 import de.frostforge.backend.tags.domain.EditTag;
 import de.frostforge.backend.tags.service.TagService;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/tags")
@@ -47,5 +52,27 @@ public class TagController {
     @DeleteMapping("/{id}")
     public void delete(@PathVariable Long id) {
         tagService.deleteById(id);
+    }
+
+    /**
+     * Handles validation exceptions in a way that they are more human readable
+     * @param ex The exception
+     * @return
+     */
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleValidationExceptions(
+            MethodArgumentNotValidException ex) {
+
+        Map<String, String> errors = new HashMap<>();
+
+        ex.getBindingResult()
+                .getAllErrors()
+                .forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return errors;
     }
 }
